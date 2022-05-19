6c3fc34087ff68c97a7738349c2cdc63b7e257b9
MinamiHotta <m.hotta@4nonome.com>
2022-05-19 18:44:56 +0900

    0519 Molecules-Plan 未完 ver.1

---
+++ src/stories/molecules-plan.stories.js
@@ -0,0 +1,6 @@
+import Molecules_Plan from"../components/molecules/plan/plan.html";
+import "../components/molecules/plan/plan.scss";
+
+export default{ title:"Molecules-plan"};
+
+export const Example = () => Molecules_Plan;
