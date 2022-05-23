b512320a26973697e135fe094692a0b10f0b74ad
MinamiHotta <m.hotta@4nonome.com>
2022-05-24 00:06:33 +0900

    0524　Organisms-News 前回とあまり変わっていないver.

--- src/stories/organisms-news.stories.js
+++ src/stories/organisms-news.stories.js
@@ -1,6 +1,6 @@
-import Item_News from "../components/organisms/news/news.html";
-import "../components/organisms/news/news.scss";
+import Item_News from "../components/organisms/events/news/news.html";
+import "../components/organisms/events/news/news.scss";
 
 export default { title: "Organisms-News" };
 
-export const Events = () => Item_News;
+export const Example = () => Item_News;
