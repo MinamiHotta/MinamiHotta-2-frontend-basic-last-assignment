47c7458c19c1257d8b65bbeebb0998bf0d1574af
MinamiHotta <m.hotta@4nonome.com>
2022-05-25 23:27:43 +0900

    0525 Stories.js Title変更

--- src/stories/molecules-message.stories.js
+++ src/stories/molecules-message.stories.js
@@ -1,6 +1,6 @@
-import Molecules_Message from"../components/molecules/message/message.html";
+import Molecules_Message from "../components/molecules/message/message.html";
 import "../components/molecules/message/message.scss";
 
-export default{ title:"Molecules-Message"};
+export default { title: "Molecules/Message" };
 
 export const Example = () => Molecules_Message;
