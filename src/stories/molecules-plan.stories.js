b2097e893fa09d634bca3be6f061443a2069ee72
MinamiHotta <m.hotta@4nonome.com>
2022-05-29 13:22:13 +0900

    0529 3次PR後

--- src/stories/molecules-plan.stories.js
+++ src/stories/molecules-plan.stories.js
@@ -1,6 +1,6 @@
-import Molecules_Plan from"../components/molecules/plan/plan.html";
+import Item_Molecules_Plan from "../components/molecules/plan/plan.html";
 import "../components/molecules/plan/plan.scss";
 
-export default{ title:"Molecules-plan"};
+export default { title: "Molecules/plan" };
 
-export const Example = () => Molecules_Plan;
+export const Example = () => Item_Molecules_Plan;
