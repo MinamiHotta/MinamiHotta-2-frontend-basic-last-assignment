12f24e9d15724ffe005ae14f713127935f499082
MinamiHotta <m.hotta@4nonome.com>
2022-06-02 17:25:02 +0900

    0602 toPR

--- src/stories/molecules-plan.stories.js
+++ src/stories/molecules-plan.stories.js
@@ -1,6 +1,6 @@
-import Item_Molecules_Plan from "../components/molecules/plan/plan.html";
+import Item_Example from "../components/molecules/plan/plan.html";
 import "../components/molecules/plan/plan.scss";
 
 export default { title: "Molecules/plan" };
 
-export const Example = () => Item_Molecules_Plan;
+export const Example = () => Item_Example;
