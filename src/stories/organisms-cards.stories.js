cefea1797009055c83450d764e22790d052a6145
MinamiHotta <m.hotta@4nonome.com>
2022-06-05 01:50:55 +0900

    0605 card-titleのはみ出しと縦並びの隙間が未

---
+++ src/stories/organisms-cards.stories.js
@@ -0,0 +1,6 @@
+import Item_Example from "../components/organisms/cards/cards.html";
+import "../components/organisms/cards/cards.scss";
+
+export default { title: "Organisms/Cards" };
+
+export const Example = () => Item_Example;
