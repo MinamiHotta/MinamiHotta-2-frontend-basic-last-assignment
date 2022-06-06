7aa4bbf95a542884d9d85dc53bdcd5d8eae132f2
MinamiHotta <m.hotta@4nonome.com>
2022-06-06 13:48:10 +0900

    0606 margin等修正ver作成

---
+++ src/stories/fix-message-margin.stories.js
@@ -0,0 +1,6 @@
+import Molecules_fixMessage from "../components/molecules/fix-message-margin/fix-message-margin.html";
+import "../components/molecules/fix-message-margin/fix-message-margin.scss";
+
+export default { title: "Molecules/fixMessage" };
+
+export const Example = () => Molecules_fixMessage;
