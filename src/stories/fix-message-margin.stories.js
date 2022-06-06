de587ccd086183b8dfdc8a338848250bce6b5934
MinamiHotta <m.hotta@4nonome.com>
2022-06-06 16:01:04 +0900

    0606 stories.js 削除

--- src/stories/fix-message-margin.stories.js
+++
@@ -1,6 +0,0 @@
-import Molecules_fixMessage from "../components/molecules/fix-message-margin/fix-message-margin.html";
-import "../components/molecules/fix-message-margin/fix-message-margin.scss";
-
-export default { title: "Molecules/fixMessage" };
-
-export const Example = () => Molecules_fixMessage;
